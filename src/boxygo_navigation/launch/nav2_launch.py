e32f717b5e62323cbefa731b4bdb77da559f8a73
Ligas10105 <ligass22@yahoo.com>
2025-12-04 22:47:56 +0100

    update

--- src/boxygo_navigation/launch/nav2_launch.py
+++ src/boxygo_navigation/launch/nav2_launch.py
@@ -1,17 +1,15 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetRemap
 from ament_index_python.packages import get_package_share_directory
 import os
 from launch.actions import IncludeLaunchDescription, TimerAction, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import SetRemap
 
 
 def generate_launch_description():
     boxygo_navigation_pkg = get_package_share_directory('boxygo_navigation')
     boxygo_localization_pkg = get_package_share_directory('boxygo_localization')
-    boxygo_gazebo_pkg = get_package_share_directory('boxygo_gazebo')
     
     map_file = os.path.join(boxygo_localization_pkg, 'maps', 'playground_map.yaml') # small_city_map.yaml / playground_map.yaml
     params_file = os.path.join(boxygo_navigation_pkg, 'config', 'nav2_params_h_2.yaml')
@@ -31,20 +29,20 @@ def generate_launch_description():
         name='ekf_filter_node',
         output='screen',
         parameters=[ekf_config, {'use_sim_time': use_sim_time}],
-    ),
+    )
 
     nav2_node = TimerAction(
-            period=5.0,
-            actions=[
-                SetRemap(src='/cmd_vel', dst='/diff_cont/cmd_vel_unstamped'),
-                IncludeLaunchDescription(
-                    PythonLaunchDescriptionSource(
-                        os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py')
-                    ),
-                    launch_arguments={'params_file': params_file, 'map': map_file, 'use_sim_time': use_sim_time, 'autostart': 'true'}.items()
+        period=5.0,
+        actions=[
+            SetRemap(src='/cmd_vel', dst='/diff_cont/cmd_vel_unstamped'),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py')
                 ),
-            ]
-        ),
+                launch_arguments={'params_file': params_file, 'map': map_file, 'use_sim_time': use_sim_time, 'autostart': 'true'}.items()
+            ),
+        ]
+    )
 
     return LaunchDescription([
         declare_use_sim_time,
